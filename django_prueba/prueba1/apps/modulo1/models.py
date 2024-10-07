18a13e2101b0e8b7a3271f3f7b54a1184f377189
Gerar22021 <Gerar2_yo@hotmail.com>
2024-10-07 19:04:46 -0300

    aaa

--- django_prueba/prueba1/apps/modulo1/models.py
+++ django_prueba/prueba1/apps/modulo1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class post(models.Model):
+    nombre = models.CharField(max_length=20, null=False, unique=True)
+    descripcion = models.TextField()
+    image = models.ImageField(upload_to='modulo1', default='categoria/imagen_default.png')
+    fecha_agregado = models.DateTimeField(auto_now_add=True)
+    
